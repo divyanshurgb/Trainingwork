42967237665ffbe4ceed69037f8f110ed880d0be
divyanshurgb <divic49@gmail.com>
2022-01-19 15:03:29 +0530

    More on UseEffect

--- UseEffect/reactapp/src/App.js
+++ UseEffect/reactapp/src/App.js
@@ -1,10 +1,18 @@
-// import React, { useState, useEffect } from 'react'
+// When to useEffect - when anytime you need a side effect, when variable change, props change, state change, components mounts, unmounts, etc.
+import React, { useState, useEffect } from 'react'
 
-// // useeffect does sideeffect whenever something happens
-// export default function App() {
-//   const [resourceType, setResourceType] = useState('posts')
-//   const [items, setItems] = useState([])
-//   console.log('render')
+export default function App() {
+  const [resourceType, setResourceType] = useState('posts')
+  const [items, setItems] = useState([])
+  console.log('render')
+
+  useEffect(() => {
+    console.log('resource changed')
+
+    return () => {
+      console.log('return from resource changed')
+    }
+  }, [resourceType])
 
   // useEffect(() => {
   //   fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
@@ -12,36 +20,41 @@
   //     .then(json => setItems(json))
   // }, [resourceType])
 
-//   return ( 
-//   <>
-//     <div>
-//       <button onClick={() => setResourceType('posts')}>Posts</button>
-//       <button onClick={() => setResourceType('posts')}>Users</button>
-//       <button onClick={() => setResourceType('posts')}>Comments</button>
-//     </div>
-//     <h1>{resourceType}</h1>
-//     {items.map(item => {
-//       return <pre>{JSON.stringify(item)}</pre>
-//     })}
-//   </>
-//   )
-// }
-
-import React, { useState, useEffect } from 'react'
-
-export default function App() {
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth)
-  
-  const handleResize = () => {
-    setWindowWidth(window.innerWidth)
-  }
-
-  useEffect(() => {
-    window.addEventListener('resize', handleResize)
-  }, [])
-
-return ( 
-    <div>  {windowWidth}  </div>
+  return ( 
+  <>
+    <div>
+      <button onClick={() => setResourceType('posts')}>Posts</button>
+      <button onClick={() => setResourceType('posts')}>Users</button>
+      <button onClick={() => setResourceType('posts')}>Comments</button>
+    </div>
+    <h1>{resourceType}</h1>
+    {items.map(item => {
+      return <pre>{JSON.stringify(item)}</pre>
+    })}
+  </>
   )
 }
+
+// import React, { useState, useEffect } from 'react'
+
+// export default function App() {
+//   const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+  
+//   const handleResize = () => {
+//     setWindowWidth(window.innerWidth)
+//   }
+
+//   useEffect(() => {
+//     window.addEventListener('resize', handleResize)
+
+//     return () => {
+//       window.removeEventListener('resize', handleResize)
+//     }
+
+//   }, [])
+
+// return ( 
+//     <div>  {windowWidth}  </div>
+//   )
+// }
 
