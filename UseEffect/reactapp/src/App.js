e2f89af7a7bc0340a2e7393ba80db98dd4cc34c6
divyanshurgb <divic49@gmail.com>
2022-01-19 14:49:04 +0530

    New update

--- UseEffect/reactapp/src/App.js
+++ UseEffect/reactapp/src/App.js
@@ -1,26 +1,47 @@
+// import React, { useState, useEffect } from 'react'
+
+// // useeffect does sideeffect whenever something happens
+// export default function App() {
+//   const [resourceType, setResourceType] = useState('posts')
+//   const [items, setItems] = useState([])
+//   console.log('render')
+
+  // useEffect(() => {
+  //   fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+  //     .then(response => response.json())
+  //     .then(json => setItems(json))
+  // }, [resourceType])
+
+//   return ( 
+//   <>
+//     <div>
+//       <button onClick={() => setResourceType('posts')}>Posts</button>
+//       <button onClick={() => setResourceType('posts')}>Users</button>
+//       <button onClick={() => setResourceType('posts')}>Comments</button>
+//     </div>
+//     <h1>{resourceType}</h1>
+//     {items.map(item => {
+//       return <pre>{JSON.stringify(item)}</pre>
+//     })}
+//   </>
+//   )
+// }
+
 import React, { useState, useEffect } from 'react'
 
-// useeffect does sideeffect whenever something happens
 export default function App() {
-  const [resourceType, setResourceType] = useState('posts')
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth)
   
-  console.log('render')
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth)
+  }
 
   useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
-      .then(response => response.json())
-      .then(json => console.log(json))
-  }, [resourceType])
+    window.addEventListener('resize', handleResize)
+  }, [])
 
-  return ( 
-  <>
-    <div>
-      <button onClick={() => setResourceType('posts')}>Posts</button>
-      <button onClick={() => setResourceType('posts')}>Users</button>
-      <button onClick={() => setResourceType('posts')}>Comments</button>
-    </div>
-    <h1>{resourceType}</h1>
-  </>
+return ( 
+    <div>  {windowWidth}  </div>
   )
 }
 
